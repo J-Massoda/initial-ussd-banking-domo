5094a2d5819ea5efb8d0a82a1c918ccb5a587588
massoda <emilienmassoda@gmail.com>
2025-12-21 00:20:09 +0100

    running live demo

--- src/routes/ussd.routes.js
+++ src/routes/ussd.routes.js
@@ -5,3 +5,25 @@ const controller = require('../ussd/controller');
 router.post('/callback', controller.handleRequest);
 
 module.exports = router;
+
+app.get('/', (req, res) => {
+  res.send('USSD Banking API is running');
+});
+
+import express from 'express';
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('USSD Banking API is running');
+});
+
+app.post('/ussd', (req, res) => {
+  res.send('USSD endpoint');
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
